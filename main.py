6ed3edda99e89a5c97d4b161f6fe64889d5ccd72
tasnim <tasnimben835@gmail.com>
2026-08-17 22:33:19 +0100

    Premier commit : assistant vocal Kiara

---
+++ main.py
@@ -0,0 +1,68 @@
+import speech_recognition as sr
+import webbrowser
+import time
+import playsound
+import os
+import random
+from gtts import gTTS
+from time import ctime
+
+
+
+r=sr.Recognizer()
+def record_audio(ask=False):
+    with sr.Microphone() as source:
+        if ask:
+            kiara_speak(ask)
+        audio=r.listen(source)
+        voice_data=''
+        try:
+            voice_data=r.recognize_google(audio)
+        except sr.UnknownValueError:
+            kiara_speak("sorry i did not get that")
+        except sr.RequestError:
+            kiara_speak("sorry,my speech service is down")
+        return voice_data
+
+
+def kiara_speak(audio_string):
+    tts=gTTS(text=audio_string, lang='en')
+    r=random.randint(1,10000000)
+    audio_file='audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        kiara_speak("my name is kiara ")
+    if 'what time is it' in voice_data:
+       kiara_speak(ctime())
+    if 'search' in voice_data:
+        search=record_audio('what do you want to search for')
+        url='https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        kiara_speak('here is what i found for '+ search)
+    if 'find location' in voice_data:
+            location=record_audio('what is the location ?')
+            url='https://google.nl/maps/place/' + location + '/&amp;'
+            webbrowser.get().open(url)
+            kiara_speak('here is the location of '+ location)
+    if 'exit' in voice_data:
+        exit()
+
+
+
+
+
+
+time.sleep(1)
+kiara_speak("how can i help you?")
+while 1:
+    voice_data=record_audio()
+    respond(voice_data)
+
+            
